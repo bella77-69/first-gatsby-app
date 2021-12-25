232fdb9217aa0987cffbbcfd38458a63ff5b0610
chantelle mckenzie <mrspasceri@hotmail.com>
2021-12-25 09:00:02 -0800

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
-      }
+        path: `${__dirname}/blog/`,
+      },
     },
+    "gatsby-plugin-mdx",
   ],
 };
